37075b098e794e665a13a841b3dbd10cc39ae263
Aleksei Statkevich <astatkevich@fb.com>
2017-05-08 03:01:02 -0700

    Add Thrift Connector

---
+++ presto-thrift-connector/src/main/java/com/facebook/presto/connector/thrift/ThriftMetadata.java
@@ -0,0 +1,201 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.connector.thrift;
+
+import com.facebook.presto.connector.thrift.annotations.ForMetadataRefresh;
+import com.facebook.presto.connector.thrift.api.PrestoThriftNullableSchemaName;
+import com.facebook.presto.connector.thrift.api.PrestoThriftNullableTableMetadata;
+import com.facebook.presto.connector.thrift.api.PrestoThriftService;
+import com.facebook.presto.connector.thrift.clientproviders.PrestoThriftServiceProvider;
+import com.facebook.presto.spi.ColumnHandle;
+import com.facebook.presto.spi.ColumnMetadata;
+import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.spi.ConnectorTableHandle;
+import com.facebook.presto.spi.ConnectorTableLayout;
+import com.facebook.presto.spi.ConnectorTableLayoutHandle;
+import com.facebook.presto.spi.ConnectorTableLayoutResult;
+import com.facebook.presto.spi.ConnectorTableMetadata;
+import com.facebook.presto.spi.Constraint;
+import com.facebook.presto.spi.SchemaTableName;
+import com.facebook.presto.spi.SchemaTablePrefix;
+import com.facebook.presto.spi.TableNotFoundException;
+import com.facebook.presto.spi.connector.ConnectorMetadata;
+import com.facebook.presto.spi.type.TypeManager;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import io.airlift.units.Duration;
+
+import javax.inject.Inject;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.Executor;
+
+import static com.facebook.presto.connector.thrift.api.PrestoThriftSchemaTableName.fromSchemaTableName;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.cache.CacheLoader.asyncReloading;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+public class ThriftMetadata
+        implements ConnectorMetadata
+{
+    private static final Duration EXPIRE_AFTER_WRITE = new Duration(10, MINUTES);
+    private static final Duration REFRESH_AFTER_WRITE = new Duration(2, MINUTES);
+
+    private final PrestoThriftServiceProvider clientProvider;
+    private final LoadingCache<SchemaTableName, Optional<ConnectorTableMetadata>> tableCache;
+
+    @Inject
+    public ThriftMetadata(
+            PrestoThriftServiceProvider clientProvider,
+            TypeManager typeManager,
+            @ForMetadataRefresh Executor metadataRefreshExecutor)
+    {
+        this.clientProvider = requireNonNull(clientProvider, "clientProvider is null");
+        requireNonNull(typeManager, "typeManager is null");
+        this.tableCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(EXPIRE_AFTER_WRITE.toMillis(), MILLISECONDS)
+                .refreshAfterWrite(REFRESH_AFTER_WRITE.toMillis(), MILLISECONDS)
+                .build(asyncReloading(new CacheLoader<SchemaTableName, Optional<ConnectorTableMetadata>>()
+                {
+                    @Override
+                    public Optional<ConnectorTableMetadata> load(SchemaTableName schemaTableName)
+                            throws Exception
+                    {
+                        requireNonNull(schemaTableName, "schemaTableName is null");
+                        return clientProvider.runOnAnyHost(client -> {
+                            PrestoThriftNullableTableMetadata thriftTableMetadata = client.getTableMetadata(fromSchemaTableName(schemaTableName));
+                            if (thriftTableMetadata.getTableMetadata() == null) {
+                                return Optional.empty();
+                            }
+                            else {
+                                ConnectorTableMetadata tableMetadata = thriftTableMetadata.getTableMetadata().toConnectorTableMetadata(typeManager);
+                                checkState(Objects.equals(schemaTableName, tableMetadata.getTable()), "requested and actual table names are different");
+                                return Optional.of(tableMetadata);
+                            }
+                        });
+                    }
+                }, metadataRefreshExecutor));
+    }
+
+    @Override
+    public List<String> listSchemaNames(ConnectorSession session)
+    {
+        return clientProvider.runOnAnyHost(PrestoThriftService::listSchemaNames);
+    }
+
+    @Override
+    public ConnectorTableHandle getTableHandle(ConnectorSession session, SchemaTableName tableName)
+    {
+        Optional<ConnectorTableMetadata> tableMetadata = tableCache.getUnchecked(tableName);
+        if (!tableMetadata.isPresent()) {
+            return null;
+        }
+        else {
+            SchemaTableName storedTableName = tableMetadata.get().getTable();
+            return new ThriftTableHandle(storedTableName.getSchemaName(), storedTableName.getTableName());
+        }
+    }
+
+    @Override
+    public List<ConnectorTableLayoutResult> getTableLayouts(
+            ConnectorSession session,
+            ConnectorTableHandle table,
+            Constraint<ColumnHandle> constraint,
+            Optional<Set<ColumnHandle>> desiredColumns)
+    {
+        ThriftTableHandle tableHandle = (ThriftTableHandle) table;
+        ThriftTableLayoutHandle layoutHandle = new ThriftTableLayoutHandle(
+                tableHandle.getSchemaName(),
+                tableHandle.getTableName(),
+                desiredColumns,
+                constraint.getSummary());
+        return ImmutableList.of(new ConnectorTableLayoutResult(new ConnectorTableLayout(layoutHandle), constraint.getSummary()));
+    }
+
+    @Override
+    public ConnectorTableLayout getTableLayout(ConnectorSession session, ConnectorTableLayoutHandle handle)
+    {
+        return new ConnectorTableLayout(handle);
+    }
+
+    @Override
+    public ConnectorTableMetadata getTableMetadata(ConnectorSession session, ConnectorTableHandle tableHandle)
+    {
+        ThriftTableHandle handle = ((ThriftTableHandle) tableHandle);
+        return getTableMetadata(new SchemaTableName(handle.getSchemaName(), handle.getTableName()));
+    }
+
+    private ConnectorTableMetadata getTableMetadata(SchemaTableName schemaTableName)
+    {
+        Optional<ConnectorTableMetadata> table = tableCache.getUnchecked(schemaTableName);
+        if (!table.isPresent()) {
+            throw new TableNotFoundException(schemaTableName);
+        }
+        else {
+            return table.get();
+        }
+    }
+
+    @Override
+    public List<SchemaTableName> listTables(ConnectorSession session, String schemaNameOrNull)
+    {
+        return clientProvider.runOnAnyHost(client -> client.listTables(new PrestoThriftNullableSchemaName(schemaNameOrNull)))
+                .stream()
+                .map(thriftSchemaTable -> new SchemaTableName(thriftSchemaTable.getSchemaName(), thriftSchemaTable.getTableName()))
+                .collect(toImmutableList());
+    }
+
+    @Override
+    public Map<String, ColumnHandle> getColumnHandles(ConnectorSession session, ConnectorTableHandle tableHandle)
+    {
+        return getTableMetadata(session, tableHandle).getColumns()
+                .stream()
+                .collect(toImmutableMap(
+                        ColumnMetadata::getName,
+                        columnMetadata -> new ThriftColumnHandle(
+                                columnMetadata.getName(),
+                                columnMetadata.getType(),
+                                columnMetadata.getComment(),
+                                columnMetadata.isHidden())));
+    }
+
+    @Override
+    public ColumnMetadata getColumnMetadata(ConnectorSession session, ConnectorTableHandle tableHandle, ColumnHandle columnHandle)
+    {
+        ThriftColumnHandle handle = ((ThriftColumnHandle) columnHandle);
+        return new ColumnMetadata(handle.getColumnName(), handle.getColumnType(), handle.getComment(), handle.isHidden());
+    }
+
+    @Override
+    public Map<SchemaTableName, List<ColumnMetadata>> listTableColumns(ConnectorSession session, SchemaTablePrefix prefix)
+    {
+        ImmutableMap.Builder<SchemaTableName, List<ColumnMetadata>> columns = ImmutableMap.builder();
+        for (SchemaTableName tableName : listTables(session, prefix.getSchemaName())) {
+            ConnectorTableMetadata tableMetadata = getTableMetadata(tableName);
+            columns.put(tableName, tableMetadata.getColumns());
+        }
+        return columns.build();
+    }
+}
---
+++ presto-thrift-connector/src/main/java/com/facebook/presto/connector/thrift/ThriftPageSource.java
@@ -0,0 +1,190 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.connector.thrift;
+
+import com.facebook.presto.connector.thrift.api.PrestoThriftNullableToken;
+import com.facebook.presto.connector.thrift.api.PrestoThriftPage;
+import com.facebook.presto.connector.thrift.api.PrestoThriftService;
+import com.facebook.presto.connector.thrift.clientproviders.PrestoThriftServiceProvider;
+import com.facebook.presto.spi.ColumnHandle;
+import com.facebook.presto.spi.ConnectorPageSource;
+import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.type.Type;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
+import static io.airlift.concurrent.MoreFutures.getFutureValue;
+import static io.airlift.concurrent.MoreFutures.toCompletableFuture;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Objects.requireNonNull;
+
+public class ThriftPageSource
+        implements ConnectorPageSource
+{
+    private final byte[] splitId;
+    private final PrestoThriftService client;
+    private final List<String> columnNames;
+    private final List<Type> columnTypes;
+    private final long maxBytesPerResponse;
+    private final AtomicLong readTimeNanos = new AtomicLong(0);
+
+    private byte[] nextToken;
+    private boolean firstCall = true;
+    private CompletableFuture<PrestoThriftPage> future;
+    private long completedBytes;
+
+    public ThriftPageSource(
+            PrestoThriftServiceProvider clientProvider,
+            ThriftConnectorConfig config,
+            ThriftConnectorSplit split,
+            List<ColumnHandle> columns)
+    {
+        // init columns
+        requireNonNull(columns, "columns is null");
+        List<String> columnNames = new ArrayList<>(columns.size());
+        List<Type> columnTypes = new ArrayList<>(columns.size());
+        for (ColumnHandle columnHandle : columns) {
+            ThriftColumnHandle thriftColumnHandle = (ThriftColumnHandle) columnHandle;
+            columnNames.add(thriftColumnHandle.getColumnName());
+            columnTypes.add(thriftColumnHandle.getColumnType());
+        }
+        this.columnNames = unmodifiableList(columnNames);
+        this.columnTypes = unmodifiableList(columnTypes);
+
+        // init config variables
+        this.maxBytesPerResponse = requireNonNull(config, "config is null").getMaxResponseSize().toBytes();
+
+        // init split
+        requireNonNull(split, "split is null");
+        this.splitId = split.getSplitId();
+
+        // init client
+        requireNonNull(clientProvider, "clientProvider is null");
+        if (split.getAddresses().isEmpty()) {
+            this.client = clientProvider.anyHostClient();
+        }
+        else {
+            this.client = clientProvider.selectedHostClient(split.getAddresses());
+        }
+    }
+
+    @Override
+    public long getTotalBytes()
+    {
+        return 0;
+    }
+
+    @Override
+    public long getCompletedBytes()
+    {
+        return completedBytes;
+    }
+
+    @Override
+    public long getReadTimeNanos()
+    {
+        return readTimeNanos.get();
+    }
+
+    @Override
+    public long getSystemMemoryUsage()
+    {
+        return 0;
+    }
+
+    @Override
+    public boolean isFinished()
+    {
+        return !firstCall && !canGetMoreData(nextToken);
+    }
+
+    @Override
+    public Page getNextPage()
+    {
+        if (future != null) {
+            if (!future.isDone()) {
+                // data request is in progress
+                return null;
+            }
+            else {
+                // response for data request is ready
+                PrestoThriftPage rowsBatch = getFutureValue(future);
+                Page result = processBatch(rowsBatch);
+                // immediately try sending a new request
+                if (canGetMoreData(nextToken)) {
+                    future = sendDataRequestInternal();
+                }
+                else {
+                    future = null;
+                }
+                return result;
+            }
+        }
+        else {
+            // no data request in progress
+            if (firstCall || canGetMoreData(nextToken)) {
+                // no data in the current batch, but can request more; will send a request
+                future = sendDataRequestInternal();
+            }
+            return null;
+        }
+    }
+
+    private boolean canGetMoreData(byte[] nextToken)
+    {
+        return nextToken != null;
+    }
+
+    private CompletableFuture<PrestoThriftPage> sendDataRequestInternal()
+    {
+        final long start = System.nanoTime();
+        ListenableFuture<PrestoThriftPage> rowsBatchFuture =
+                client.getRows(splitId, columnNames, maxBytesPerResponse, new PrestoThriftNullableToken(nextToken));
+        rowsBatchFuture.addListener(() -> readTimeNanos.addAndGet(System.nanoTime() - start), directExecutor());
+        return toCompletableFuture(rowsBatchFuture);
+    }
+
+    private Page processBatch(PrestoThriftPage rowsBatch)
+    {
+        firstCall = false;
+        nextToken = rowsBatch.getNextToken();
+        Page page = rowsBatch.toPage(columnTypes);
+        if (page != null) {
+            completedBytes += page.getSizeInBytes();
+        }
+        return page;
+    }
+
+    @Override
+    public CompletableFuture<?> isBlocked()
+    {
+        return future == null || future.isDone() ? NOT_BLOCKED : future;
+    }
+
+    @Override
+    public void close()
+            throws IOException
+    {
+        if (future != null) {
+            future.cancel(true);
+        }
+        client.close();
+    }
+}
